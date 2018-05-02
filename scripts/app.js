5e96f89af5400341f3aa7212ff09fa3c9a9447ac
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-05-02 16:26:26 -0700

    feat) Add all prices to graph

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,151 @@
 if (!window.app) {
   window.app = {
-    Load: _Load,
-    GetStockPrices: _GetStockPrices,
-    RenderPrices: _RenderPrices,
-    RenderGraph: _RenderGraph,
-    Prices: {
+    load,
+    getStockPrices,
+    renderPrices,
+    renderGraph,
+    prices: {
       'MSFT': []
     }
   };
 }
 
-$('#go').click(() => {
-  app.GetStockPrices($("#stockTicker").val());
+$("#stock-ticker").keyup(function(event) {
+  if (event.keyCode === 13) {
+      $("#go").click();
+  }
 });
 
-function _Load() {
-  app.GetStockPrices("MSFT");
+$('#go').click(() => {
+  app.getStockPrices($("#stock-ticker").val());
+});
+
+
+function load() {
+  app.getStockPrices("MSFT");
 }
 
-function _GetStockPrices(ticker) {
+function getStockPrices(ticker) {
   var formattedTicker = ticker.toUpperCase(); // Keep stock symbol format consistent
   var urlTemplate = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={ticker}&apikey=ES0RLUA5ZTYI9VFC";
   var urlToFetch = urlTemplate.replace("{ticker}", formattedTicker);
 
-  if (app.Prices[formattedTicker] && app.Prices[formattedTicker].length > 0) {
-    app.RenderPrices(app.Prices[formattedTicker]);
-    app.RenderGraph(app.Prices[formattedTicker]);
+  if (app.prices[formattedTicker] && app.prices[formattedTicker].length > 0) {
+    app.renderPrices(app.prices[formattedTicker]);
+    app.renderGraph(app.prices[formattedTicker]);
   } else {
-    app.Prices[formattedTicker] = [];
+    app.prices[formattedTicker] = [];
 
     $.getJSON({
       url: urlToFetch, success: function (result) {
         var fetchedPrices = result["Time Series (Daily)"];
+        console.log(fetchedPrices);
 
         for (var p in fetchedPrices) {
-          app.Prices[formattedTicker].push({ "DateOfPrice": p, ClosePrice: fetchedPrices[p]["4. close"] });
+          app.prices[formattedTicker].push({
+            dateOfPrice: p,
+            open: fetchedPrices[p]["1. open"],
+            high: fetchedPrices[p]["2. high"],
+            low: fetchedPrices[p]["3. low"],
+            close: fetchedPrices[p]["4. close"],
+          });
         }
 
-        app.RenderPrices(app.Prices[formattedTicker]);
-        app.RenderGraph(app.Prices[formattedTicker]);
+        app.renderPrices(app.prices[formattedTicker]);
+        app.renderGraph(app.prices[formattedTicker]);
       }
     });
   }
 }
 
-function _RenderPrices(listOfPrices) {
+function renderPrices(listOfPrices) {
   var rows = [];
   for (var i = 0; i < listOfPrices.length; i++) {
     rows.push(React.createElement(StockPriceRow, listOfPrices[i]));
   }
   ReactDOM.render(
     rows,
-    document.getElementById('priceList')
+    document.getElementById('price-list')
   );
 }
 
-function _RenderGraph(prices) {
-  var closePrices = prices.map(item => item.ClosePrice);
-  var dates = prices.map(item => item.DateOfPrice);
-  console.log(closePrices);
-  var trace = {
+function renderGraph(prices) {
+  //   var traceObject = {
+  //     dates: [],
+  //     close: [],
+  //     open: [],
+  //     low: [],
+  //     hight: []
+  //   };
+
+  //   for (var i = 0; i < prices.length; i++) { 
+
+  //   }
+
+  //   var lines = prices.map((item) => {
+
+  //     if (! traceObject[item]) {
+  //       traceObject[key] = prices.map(item => item.key)
+  //     }
+  //   });
+  // console.log(lines);
+
+  // var graphLines = prices.map((item)) => {
+  //   if (item !== 'dateOfPrice') {
+  //    return {
+  //       x: item.dateOfPrice,
+  //       y: prices.map(price => ,
+  //       type: 'scatter'
+  //     }
+  //   };
+  // });
+
+  var dates = prices.map(item => item.dateOfPrice);
+  var priceHigh = prices.map(item => item.high);
+  var priceLow = prices.map(item => item.low);
+  var closePrice = prices.map(item => item.close);
+  var openPrice = prices.map(item => item.open);
+
+  var high = {
     x: dates,
-    y: closePrices
+    y: priceHigh,
+    name: 'High',
+    mode: 'lines+markers'
   }
 
-  Plotly.newPlot('priceGraph', [trace]);
+  var low = {
+    x: dates,
+    y: priceLow,
+    name: 'Low',
+    mode: 'lines+markers'
+  }
+
+  var close = {
+    x: dates,
+    y: closePrice,
+    name: 'Close',
+    mode: 'lines+markers'
+  }
+
+  var open = {
+    x: dates,
+    y: openPrice,
+    name: 'Open',
+    mode: 'lines+markers'
+  }
+
+  Plotly.newPlot('price-graph', [high, low, open, close]);
 }
 
 class StockPriceRow extends React.Component {
   render() {
     var cells = [];
-    var priceElement = React.createElement("td", null, this.props.DateOfPrice);
+    var priceElement = React.createElement("td", null, this.props.dateOfPrice);
     cells.push(priceElement);
-    var dateElement = React.createElement("td", null, this.props.ClosePrice);
+    var dateElement = React.createElement("td", null, this.props.close);
     cells.push(dateElement);
     return React.createElement('tr', null, cells);
   }
 }
 
-$(app.Load());
+$(app.load());
