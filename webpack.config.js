91627d1ee761e1bb9b7f970295ef384dbbe28c0f
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-05-04 11:43:26 -0700

    chore) Remove jquery

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  devtool: 'eval-cheap-module-source-map',
+  node: {
+    console: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty'
+  },
+  watch: true
 };
