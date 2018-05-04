f5098bd543fd048504f9e98f97bdd2e08a5ca23b
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-05-04 07:03:35 -0700

    Add Node, webpack, babel

---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+const sinon = require('sinon');
+const getStockPrices = require('../scripts/app');
+
+describe('getStockPrices', () => {
+  it('should do stuff', () => {
+    console.log(getStockPrices);
+  });
+});
